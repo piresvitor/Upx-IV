35c3eee60d0b373cf81cb31d2749258a11401699
Júlia Mofardini <juliamofardini@hotmail.com>
2025-11-04 23:01:48 -0300

    refactor: remove Features component from Home page layout

--- frontend/UPX-IV/src/pages/home.tsx
+++ frontend/UPX-IV/src/pages/home.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/features/home/Hero";
-import Features from "@/features/home/Features";
 import Stats from "@/features/home/Stats";
 import HowItWorks from "@/features/home/HowlWorks";
 import CTA from "@/features/home/CTA";
@@ -9,9 +8,9 @@ export default function Home() {
   return (
     <div className="gap-5">
       <Hero />
-      <Features />
-      <Stats />
       <HowItWorks />
+
+      <Stats />
       <CTA />
       <Footer />
     </div>
